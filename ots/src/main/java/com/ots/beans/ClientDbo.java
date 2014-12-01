06257d0234921ffcdb20580cfde84e1cdef505aa
srinath1691 <srinath1691@gmail.com>
2014-12-01 01:15:22 -0600

    trader dao and addition to client dao

---
+++ ots/src/main/java/com/ots/beans/ClientDbo.java
@@ -0,0 +1,27 @@
+package com.ots.beans;
+
+public class ClientDbo {
+	
+	private String clientId;
+	private double quantiy;
+	private double credit;
+	
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public double getQuantiy() {
+		return quantiy;
+	}
+	public void setQuantiy(double quantiy) {
+		this.quantiy = quantiy;
+	}
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+}
--- ots/src/main/java/com/ots/dao/TraderDAO.java
+++ ots/src/main/java/com/ots/dao/TraderDAO.java
@@ -8,79 +8,35 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.ots.beans.ClientDbo;
-import com.ots.beans.PaymentForTxn;
 import com.ots.beans.Txn;
 import com.ots.beans.TxnInfo;
 import com.ots.beans.TxnLog;
 import com.ots.util.MySqlExecute;
 
 @Component
-public class ClientDAO {
+public class TraderDAO {
 
-	static final Logger log = Logger.getLogger(ClientDAO.class);
+	static final Logger log = Logger.getLogger(TraderDAO.class);
 
-	//Initiate transaction
+	//Initiate transaction for a client.
 	public void makeTxn (Txn txn) {
 
 		String query = "insert into transaction (`Txn_quantity`, `Txn_type`, `Txn_comsn_type`,"
-					+ "`Txn_Client_Id`, `Txn_total_comsn`, `Txn_cost`) values"
+					+ "`Txn_Client_Id`, `Txn_total_comsn`, `Txn_cost`, `Txn_Trader_Id`) values"
 					+ "("+txn.getQuantity()+",'"+txn.getType()+"','"+txn.getComsnType()+"','"
-					+txn.getClientId()+"',"+txn.getTotalComsn()+","+txn.getTxnCost()+")";
+					+txn.getClientId()+"',"+txn.getTotalComsn()+","+txn.getTxnCost()+ ",'"+txn.getTraderId()+"')";
 
 		try {
 			MySqlExecute.executeUpdateMySqlQuery(query);
 		} 
 		catch (Exception e) {
-			log.error("Error in creating transation " + e);
-		}
-	}
-	
-	//Can use the method view oil cash reserves. check in controller itself whether the amount n 
-	//clientdbo is greater than the amount of oil to be sold
-	
-	//Check if oil reserves are sufficient
-	/*public boolean CheckOilReserves(ClientDbo cdbo) {
-		//String query = "SELECT `Clientdbo_quantity` FROM `clientdbo` WHERE clientdbo_id = '" + + "';";
-		try {
-			ResultSet rs = MySqlExecute.executeMySqlQuery(query);
-			if (cdbo.getQuantiy() > txnamnt) {
-				log.debug("Oil reserves = "+rs.getDouble(1));
-				return true;
-			}
-			else
-				return false;
-		} 
-		catch (Exception e) {
-			log.error("Error in checking oil reserves "+e);
-			return false;
-		}
-	}*/
-	
-	//Make payment for transaction
-	public void Payment(PaymentForTxn payment){
-		String query = "insert into `payment_info` (`Payment_amount`,`Payment_txn_id`)"
-				+ " values ("+payment.getPaymentAmount()+","+payment.getTxnId()+",'" +payment.getClientId()+ ");";
-		
-		try{
-			MySqlExecute.executeUpdateMySqlQuery(query);
-		}
-		catch(Exception e){
-			log.error("Error in payment "+e);
-		}
-		
-		query = "update `Transaction` set `Txn_Cost_Paid` = "+payment.getRemainingBalance()+"where `txn_id` = "+payment.getTxnId() + ";";
-		
-		try{
-			MySqlExecute.executeUpdateMySqlQuery(query);
-		}
-		catch(Exception e){
-			log.error("Error in deducting cost paid in transaction after payment "+e);
+			log.error("Error in creating transation for client " + txn.getClientId() + " " + e);
 		}
 	}
 	
 	//View pending transactions
-	public List<Txn> ViewPendingTxn(String clientId){
-		String query = "select * from transaction where `Txn_Client_Id` = " + clientId + ";";
+	public List<Txn> ViewPendingTxn(){
+		String query = "select * from transaction";
 		try{
 			ResultSet rs = MySqlExecute.executeMySqlQuery(query);
 			if(rs.next()){
@@ -109,7 +65,7 @@ public class ClientDAO {
 		}
 	}
 	
-	//delete pending transactions and their info from payment table. Update client credit. Insert into transaction log table.
+	//delete pending transactions and their info from payment table. Update client credit.
 	public void DeleteTxn(TxnInfo txninfo){
 		String query = "delete from transaction where `txn_id` = " + txninfo.getTxnId() + ";";
 		
@@ -121,22 +77,23 @@ public class ClientDAO {
 		}
 		
 		query = "delete from payment_info where payment_txn_id = " + txninfo.getTxnId() + ""
-				+ "and payment_client_id = '" + txninfo.getClientId()+ "';";
+				+ "and payment_client_id = '"+txninfo.getClientId()+"';";
 		try{
-		MySqlExecute.executeUpdateMySqlQuery(query);
+			MySqlExecute.executeUpdateMySqlQuery(query);
 		}
 		catch(Exception e){
 			log.error("Error in deleting payment for that transaction " + e);
 		}
 		
 		query = "update clientdbo set clientdbo_quantity = " + txninfo.getTxnamnt()
-				+ "where clientdbo_id = '" + txninfo.getClientId() + "';";
+				+ "where clientdbo_id = '"+txninfo.getClientId()+"';";
+		
 		try{
 			MySqlExecute.executeUpdateMySqlQuery(query);
 		}
 		catch(Exception e){
 			log.error("Error in updating the value of transaction amount in transactio table" + e);
-		}		
+		}
 	}
 	
 	//Insert into transaction log after deleting from transaction
@@ -145,7 +102,7 @@ public class ClientDAO {
 				+ " `Txn_Log_Commission_Type`, `Txn_Log_status`, `Txn_log_Trader_id`, `Txn_log_cost`, `Txn_log_total_comsn`) "
 				+ "values (" + txnlog.getTxnLogId() + ",'" +txnlog.getClientId() + "'," + txnlog.getTxnDate() + ","
 				+txnlog.getQuantity() + ",'" + txnlog.getTxnType() + "','" + txnlog.getComsnType() + "','"	
-				+txnlog.getLogStatus() + "','" + txnlog.getTraderId()+ "'," +txnlog.getTxnCost() +","+ txnlog.getComsnCost() +")";
+				+txnlog.getLogStatus() + "','" + txnlog.getTraderId()+ "'," +txnlog.getTxnCost() +",'"+ txnlog.getComsnCost() +"')";
 		
 		try{
 			MySqlExecute.executeUpdateMySqlQuery(query);
@@ -155,39 +112,9 @@ public class ClientDAO {
 		}
 	}
 	
-	//View payments done for a transaction of a client
-	public List<PaymentForTxn> ViewPayment(TxnInfo txninfo){
-		String query = "select * from Payment_info where payment_txn_id = " + txninfo.getTxnId()+ " "
-				+ "and payment client_id = '" + txninfo.getClientId() +"';";
-		try{
-			List<PaymentForTxn> payments = new ArrayList<PaymentForTxn>();
-			PaymentForTxn payment = new PaymentForTxn();
-			ResultSet rs = MySqlExecute.executeMySqlQuery(query);
-			if(rs.next()){
-				while(rs.next()){
-					payment.setPaymentId(rs.getInt(1));
-					payment.setPaymentDate(rs.getDate(2));
-					payment.setPaymentAmount(rs.getDouble(3));
-					payment.setTxnId(rs.getInt(4));
-					payment.setStatus(rs.getString(5));
-					payment.setTraderId(rs.getString(6));
-					payment.setClientId(rs.getString(7));
-					payments.add(payment);
-				}
-				return payments;
-			}
-			else
-				return null;
-		}
-		catch(Exception e){
-			log.error("Error in fetching data from payment info " + e);
-			return null;
-		}
-	}
-	
-	//View all past transactions
-	public List<TxnLog> ViewTxns(String clientId){
-		String query = "select * from transaction_log where `txn_log_client_id` = '" + clientId + "';";
+	//View all past transactions of all clients
+	public List<TxnLog> ViewTxns(){
+		String query = "select * from transaction_log";
 		try{
 			ResultSet rs = MySqlExecute.executeMySqlQuery(query);
 			if(rs.next()){
